a41d2d5e614e14f567e1e804f831408aeb65ad79
ThisNarciss <tyomaxxx000@gmail.com>
2023-08-23 16:07:45 +0300

    style: add home page and her style

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import { SharedLayout } from "@/components/layout/SharedLayout";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <SharedLayout>
+      <Component {...pageProps} />
+    </SharedLayout>
+  );
+}
